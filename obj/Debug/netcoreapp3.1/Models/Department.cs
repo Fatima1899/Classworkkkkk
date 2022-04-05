ea7810d9e762dd7d7fd76fb7950d3c9867eb7356
IsmailovaFatima <99797429+Fatima1899@users.noreply.github.com>
2022-04-05 12:43:22 +0400

    Add files via upload

---
+++ obj/Debug/netcoreapp3.1/Models/Department.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classworkkk.Models
+{
+    class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List <Employee> _employees;
+        public Department()
+        {
+            _employees = new List<Employee>();
+        }
+
+        public void Add(Employee employee)
+        {
+            _employees.Add(employee);
+        }
+    }
+}
---
+++ obj/Debug/netcoreapp3.1/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classworkkk.Models
+{
+    class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Id:{Id}\nName:{Name}\nSalary:{Salary}");
+        }
+    }
+}
